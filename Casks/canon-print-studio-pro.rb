cce1d55b521ddb54a2a8a90f21f0085e1fb13489
John Stader <47798010+jstader@users.noreply.github.com>
2020-11-11 17:03:06 -0500

    Corrected shasum

--- Casks/canon-print-studio-pro.rb
+++ Casks/canon-print-studio-pro.rb
@@ -1,6 +1,6 @@
 cask "canon-print-studio-pro" do
   version "2.2.4"
-  sha256 "9d02496b46d5e2d706734b892041a0d6f1bebc186d533cb00674767d727b3788"
+  sha256 "d7ace5f94e379b62f7345ec693510d4453c3c6a481639c91bbd7c3396de2f936"
 
   # gdlp01.c-wss.com/gds/ was verified as official when first introduced to the cask
   url "https://gdlp01.c-wss.com/gds/6/0200006106/01/mpsp-mac-#{version.dots_to_underscores}-ea10_3.dmg"
